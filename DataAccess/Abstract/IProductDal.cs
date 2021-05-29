555606465f6b6ded41dd95fe489265dce88cf9b8
Oğuzhan Turgut <oguz.turgut32@gmail.com>
2021-05-29 22:51:19 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product prodcut);
+        void Update(Product prodcut);
+        void Delete(Product prodcut);
+
+        List<Product> GetAllByCategory(int categoryId);
+
+    }
+}
